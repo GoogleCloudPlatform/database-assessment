8c401fe193305c7ba2ec23e5dfa7059acbe4a578
Eri Santos <erisantos@google.com>
2021-05-12 12:24:39 -0400

    WIP working version. Testing needed

--- db-assessment/rules_engine.py
+++ db-assessment/rules_engine.py
@@ -62,18 +62,21 @@ def runRules(transformerRules, dataFrames):
                 transformerResults[ruleItem] = {'Status': EXECUTEDSTATUS, 'Result Value': createTransformersVariable(transformerRules[ruleItem])}
                 transformersParameters[transformerRules[ruleItem]['action_details']['varname']] = transformerResults[ruleItem]['Result Value']
 
-            # In case of any issue the rule will be marked as FAILEDSTATUS
             except:
+                # In case of any issue the rule will be marked as FAILEDSTATUS
                 transformerResults[ruleItem] = {'Status': FAILEDSTATUS, 'Result Value': None}
                 transformersParameters[transformerRules[ruleItem]['action_details']['varname']] = None
 
         elif str(transformerRules[ruleItem]['type']).upper() in ("NUMBER","FREESTYLE") and str(transformerRules[ruleItem]['action']).upper() == "ADD_COLUMN":
         # transformers.json asking to add a column that is type number meaning it can be a calculation and the column to be added is NUMBER too
 
+            print ('\nProcessing NUMBER/FREESTYLE and ADD_COLUMN.\n')
             dataFrames[transformerRules[ruleItem]['action_details']['dataframe_name']][transformerRules[ruleItem]['action_details']['column_name']] = execStringExpression(stringExpression,iferrorExpression, dataFrames)
 
         elif str(transformerRules[ruleItem]['type']).upper() == "FREESTYLE" and str(transformerRules[ruleItem]['action']).upper() == "CREATENEWDATAFRAME":
+        # 
 
+            print ('\nProcessing FREESTYLE and CREATENEWDATAFRAME.\n')
             dataFrames[transformerRules[ruleItem]['action_details']['dataframe_name']] = execStringExpression(stringExpression,iferrorExpression, dataFrames)
 
 
@@ -82,7 +85,7 @@ def runRules(transformerRules, dataFrames):
 def execStringExpression(stringExpression,iferrorExpression, dataFrames):
 
     try:
-        res = eval (stringExpression
+        res = eval (stringExpression)
     except:
         res = eval (iferrorExpression)
 
@@ -121,7 +124,7 @@ def getDataFrameFromCSV(csvFileName,skipRows):
     try:
         df = pd.read_csv(csvFileName, skiprows=skipRows)
     except:
-        print ('\n\n\n\nThe filename {} is empty.'.format(csvFileName))
+        print ('\n\n\n\nThe filename {} is empty.\n\n'.format(csvFileName))
         return False
 
     return df
@@ -214,11 +217,84 @@ def getReShapedDataframe(df, transformersParameters):
 #   1	1	18	19	400	420
 #   1	2	18	18	390	400
 
+    # Columns that will remain in a row format as indexes
+    frozenIndex = []
+    frozenIndex = transformersParameters['INDEX_COLUMNS']
+
+    # Column in which its content will be pivoted to columns
+    # For example: TARGET_COLUMN = 'IOPS'
+    targetColumn = ''
+    targetColumn = transformersParameters['TARGET_COLUMN']
+
+    # Values refered to the TARGET_COLUMN that will be shown (as second level column)
+    # For example: TARGET_COLUMN = 'IOPS' & TARGET_STATS_COLUMNS = 'AVG' THEN it means that we will get AVG IOPs
+    targetStatsColumn = []
+    targetStatsColumn = transformersParameters['TARGET_STATS_COLUMNS']
+
+    # Pivoting daframe following the parameters given
+    pivoted_df = df.pivot(index=frozenIndex, columns=targetColumn, values=targetStatsColumn)
+
+    # Getting Columns names and levels to change it
+    multiIndexColumns = pivoted_df.columns
+
+    # Function to change dataframe column names accordingly with the parameters in transformersParameters['FROM_TO_ROWS_TO_COLUMNS']
+    multiIndexColumns = getNewNamesFromMultiColumns(transformersParameters['FROM_TO_ROWS_TO_COLUMNS'], multiIndexColumns)
+
+    # Changing columns and its levels
+    pivoted_df.columns = multiIndexColumns
+
+    # Resetting MultiIndex Frozen
+    pivoted_df.reset_index(inplace=True)
+
+    return pivoted_df
+
+def getNewNamesFromMultiColumns(newNamesMapping, multiIndex):
+
+    # Turning a tuple into a list in order to be changed
+    multiIndex = list(multiIndex)
+
+    for index in range(len(multiIndex)):
+
+        if newNamesMapping.get(multiIndex[index][1]) is not None:
+
+            # Turning a tuple into a list in order to be changed
+            tempList = list(multiIndex[index])
+            tempList[1] = newNamesMapping.get(multiIndex[index][1])
+            # After the change tuning it back into a tuple
+            multiIndex[index] = tuple(tempList)
+
+    # Retuning a tuple again
+    return tuple(multiIndex)
+
+
+
+'''def getReShapedDataframe(df, transformersParameters):
+# Function to get a dataframe in one format and reshape it to another one that would make a lot simpler to create rules on.
+# Input dataframe to be reshaped example:
+#
+# FROM:
+#   DBID	HOUR	METRIC	PERC50	PERC90	PERC95
+#   1	0	Active Sessions	15	20	22
+#   1	1	Active Sessions	14	18	19
+#   1	2	Active Sessions	13	18	18
+#   1	0	User Transaction Per Sec	369	450	460
+#   1  	1	User Transaction Per Sec	301	400	420
+#   1	2	User Transaction Per Sec	280	390	400
+#   1	0	Physical Reads	904	1405	1500
+#   1	1	Physical Reads	1050	1589	1600
+#   1	2	Physical Reads	1120	1400	1450
+#
+# TO: (Using a from/to: Active Sessions == AAS, User Transaction Per Sec == UT)
+#   DBID	HOUR	AAS_PERC90	AAS_PERC95	UT_PERC90	UT_PER95
+#   1	0	20	22	450	460
+#   1	1	18	19	400	420
+#   1	2	18	18	390	400
+
 
     hours = df.drop_duplicates(subset=[transformersParameters['CLUSTER_COLUMN']])[transformersParameters['CLUSTER_COLUMN']].tolist()
     metrics = transformersParameters['FROM_TO_ROWS_TO_COLUMNS']
 
-    newShapeDataframe = df[transformersParameters['KEEP_COLUMNS']].copy()
+    newShapeDataframe = df[transformersParameters['INDEX_COLUMNS']].copy()
     newBaseShapeDataframe = newShapeDataframe.drop_duplicates()
     newBaseShapeDataframe.reset_index()
 
@@ -284,7 +360,7 @@ def appendListOfDataframes(dataframesDict):
         df.append(dataframesDict[dfIndex])
         count = count + 1
 
-    return df
+    return df'''
 
 if __name__ == '__main__':
 
