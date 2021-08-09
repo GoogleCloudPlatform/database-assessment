f40923e3859fad7d3b0459261d074936be33f8ce
Eri Santos <erisantos@google.com>
2021-08-09 12:14:59 -0400

    Fixed import for script 010

--- db_assessment/import_db_assessment.py
+++ db_assessment/import_db_assessment.py
@@ -387,10 +387,8 @@ def importCSVToBQ(gcpProjectName,bqDataset,tableName,fileName,skipLeadingRows,au
     
 
     with open(fileName, "rb") as source_file:
-        
 
         load_job = client.load_table_from_file(source_file, table_id, job_config=job_config)
-
 
 
     try:
