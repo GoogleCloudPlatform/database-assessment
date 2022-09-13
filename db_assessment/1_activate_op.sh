223c97360afea8382ea65240aa6b16a44a01f366
Cody Fincher <codyfincher@google.com>
2022-09-13 16:21:41 -0500

    conditionally create env

--- db_assessment/1_activate_op.sh
+++ db_assessment/1_activate_op.sh
@@ -2,9 +2,11 @@
 set -eo pipefail 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASE_DIR="${SCRIPT_DIR}/.."
-VENV_DIR="${SCRIPT_DIR}/../op-venv"
+export VENV_DIR="${SCRIPT_DIR}/../op-venv"
+export VENV_EXISTS=$(cd $SCRIPT_DIR && python3 -c "if __import__('pathlib').Path('../op-venv/bin/activate').exists(): print('yes')")
 
-python3 -m venv ${VENV_DIR}
+if [ $VENV_EXISTS ]; then echo "Existing environment found"; fi
+if [ ! $VENV_EXISTS ]; then python3 -m venv ${VENV_DIR}; fi
 source ${VENV_DIR}/bin/activate
 pip3 install pip wheel setuptools --upgrade
 pip3 install .
