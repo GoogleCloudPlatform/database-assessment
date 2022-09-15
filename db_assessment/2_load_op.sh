36248af348feae54a010dab9c7d9d30195ac89c2
Shane Borden <shaneborden@google.com>
2022-09-15 20:07:04 +0000

    Updated method for determining output and script directories, replaced hard coded project name and stopped failing on if the BQ dataset was already present

--- db_assessment/2_load_op.sh
+++ db_assessment/2_load_op.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASE_DIR="${SCRIPT_DIR}/.."
 VENV_DIR="${SCRIPT_DIR}/../.venv"
 
-bq mk -d --data_location=${DSLOC} ${DSNAME}
+bq mk --dataset --force=TRUE --data_location=${DSLOC} ${DSNAME}
 for COLID in $(ls -1 ${OP_LOG_DIR}/opdb*| rev | cut -d '.' -f 2 | rev | sort | uniq)
 do
 ${VENV_DIR}/bin/optimus-prime -sep "${COLSEP}" -dataset ${DSNAME} -fileslocation ${OP_LOG_DIR} -projectname ${PROJECTNAME} -collectionid ${COLID} | tee ${SCRIPT_DIR}/opload-${DSNAME}-${COLID}.log
