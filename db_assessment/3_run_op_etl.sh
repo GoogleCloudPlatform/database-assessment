234f703c0be24ce783b80e993087d4aebd9e6cd2
Shane Borden <shaneborden@google.com>
2022-09-19 16:48:27 +0000

    move scripts to common scripts dir in root of project

--- db_assessment/3_run_op_etl.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BASE_DIR="${SCRIPT_DIR}/.."
-sed "s/projectID.dataset/${PROJECTNAME}.${DSNAME}/g" $SCRIPT_DIR/op_etl_template.sql > $SCRIPT_DIR/op_etl_${DSNAME}.sql
-bq query  --use_legacy_sql=false < $SCRIPT_DIR/op_etl_${DSNAME}.sql  | tee op_etl_${DSNAME}.log
-echo
-echo A log of this process is available at op_etl_${DSNAME}.log
