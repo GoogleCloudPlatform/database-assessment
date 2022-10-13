495457636fc2943693ac7851e3fa1f9d4dadcb64
Warren Puziewicz <warrenpuz@google.com>
2022-10-13 22:07:59 +0000

    Handle embedded pipe characters in data.

--- db_assessment/dbSQLCollector/extracts/dbparameters.sql
+++ db_assessment/dbSQLCollector/extracts/dbparameters.sql
@@ -8,9 +8,9 @@ SELECT '&&v_host'
        || '&&v_hora'                                   AS pkey,
        inst_id,
        &v_a_con_id AS con_id,
-       REPLACE(name, ',', '/')                         name,
-       TRANSLATE(REPLACE(SUBSTR(value, 1, 60), ',', '/'), chr(10)||chr(13) , '  ')         value,
-       TRANSLATE(REPLACE(SUBSTR(&v_dbparam_dflt_col, 1, 30), ',', '/'), chr(10)||chr(13), '  ') default_value,
+       replace(name, chr(39), chr(34))   name,
+       TRANSLATE(SUBSTR(value, 1, 60), chr(124)||chr(10)||chr(13)||chr(39), ' ')         value,
+       TRANSLATE(SUBSTR(&v_dbparam_dflt_col, 1, 30), chr(124)||chr(10)||chr(13)||chr(39), ' ')  default_value,
        isdefault
 FROM   gv$parameter a
 ORDER  BY 2,3 )
