38e13f314b733f07ff37578de5f07e1cdd82b3f1
Cody Fincher <codyfincher@google.com>
2022-10-14 03:18:10 +0000

    wip

---
+++ src/server/dbma/transformer/rules_schema.py
@@ -0,0 +1,59 @@
+from enum import Enum
+from typing import List
+
+from pydantic import UUID4
+
+
+class OpKeyLog:
+    key: UUID4
+
+
+class RuleAction(str, Enum):
+    CREATE = "CREATE"
+    EXECUTE_SQL = "DISABLED"
+    ADD_OR_UPDATE_COLUMN = "ADD_OR_UPDATE_COLUMN"
+
+
+class RuleActionType(str, Enum):
+    FREESTYLE = "FREESTYLE"
+
+
+class RuleActionStore(str, Enum):
+    CREATE = "CREATE"
+    EXECUTE_SQL = "DISABLED"
+
+
+class TransformerStatus(str, Enum):
+    ENABLED = "ENABLED"
+    DISABLED = "DISABLED"
+
+
+class TableSchema:
+    """Represents a table schema that can be attached to a version"""
+
+
+class TransformerParameter:
+    """Represents a table schema that can be attached to a version"""
+
+
+class TransformerRule:
+    """Represents a table schema that can be attached to a version"""
+
+    priority: int = 0
+    applies_to_script_version
+    execution_group: int
+    status: TransformerStatus
+    action
+
+
+class TransformerConfig:
+    table_schemas: List[TableSchema] = []
+    parameters: List[TransformerParameter] = []
+    rules: List[TransformerRule] = []
+
+
+class TransformerActionDetail:
+    priority: int = 0
+    applies_to_script_version
+    execution_group: int
+    status: TransformerStatus
