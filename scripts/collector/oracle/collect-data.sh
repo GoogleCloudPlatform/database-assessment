a2c7e0e1009b75b4031084ba9492233063749845
Cody Fincher <204685+cofin@users.noreply.github.com>
2023-06-02 15:20:30 +0000

    chore: bump version

--- scripts/collector/oracle/collect-data.sh
+++ scripts/collector/oracle/collect-data.sh
@@ -16,7 +16,7 @@
 
 ### Setup directories needed for execution
 #############################################################################
-OpVersion="4.3.5"
+OpVersion="4.3.6"
 
 LOCALE=$(echo $LANG | cut -d '.' -f 1)
 export LANG=C
