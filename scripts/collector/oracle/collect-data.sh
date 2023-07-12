f5ea9f6970c6e0f871df0d2603579b686e8a81ff
Cody Fincher <204685+cofin@users.noreply.github.com>
2023-07-12 01:30:26 +0000

    chore: bump to 4.3.9

--- scripts/collector/oracle/collect-data.sh
+++ scripts/collector/oracle/collect-data.sh
@@ -16,7 +16,7 @@
 
 ### Setup directories needed for execution
 #############################################################################
-OpVersion="4.3.8"
+OpVersion="4.3.9"
 
 LOCALE=$(echo $LANG | cut -d '.' -f 1)
 export LANG=C
