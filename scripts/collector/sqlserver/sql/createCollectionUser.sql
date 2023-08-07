152257947460e231e45412f896a053739eec32b9
Shane Borden <shaneborden@google.com>
2023-08-07 14:06:58 -0400

    Correct for reduced permissions

--- scripts/collector/sqlserver/sql/createCollectionUser.sql
+++ scripts/collector/sqlserver/sql/createCollectionUser.sql
@@ -17,21 +17,45 @@ limitations under the License.
 
 SET NOCOUNT ON;
 SET LANGUAGE us_english;
+
+DECLARE @dbname VARCHAR(50);
+DECLARE @COLLECTION_USER VARCHAR(256);
+DECLARE db_cursor CURSOR FOR 
+SELECT name
+FROM MASTER.sys.databases 
+WHERE name NOT IN ('model','msdb','distribution','reportserver', 'reportservertempdb','resource','rdsadmin')
+AND state = 0;
+
 USE [master]
 IF NOT EXISTS 
     (SELECT name  
      FROM master.sys.server_principals
      WHERE name = N'$(collectionUser)')
+	BEGIN
+		CREATE LOGIN [$(collectionUser)] WITH PASSWORD=N'$(collectionPass)', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+	END
 BEGIN
-    CREATE LOGIN [$(collectionUser)] WITH PASSWORD=N'$(collectionPass)', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
-END
-GRANT VIEW SERVER STATE TO N'$(collectionUser)'
-GO
-GRANT SELECT ALL USER SECURABLES TO N'$(collectionUser)'
-GO
-GRANT VIEW ANY DATABASE TO N'$(collectionUser)'
-GO
-GRANT VIEW ANY DEFINITION TO N'$(collectionUser)'
-GO
-GRANT VIEW SERVER STATE TO N'$(collectionUser)'
-GO
+	GRANT VIEW SERVER STATE TO [$(collectionUser)]
+	GRANT SELECT ALL USER SECURABLES TO [$(collectionUser)]
+	GRANT VIEW ANY DATABASE TO [$(collectionUser)]
+	GRANT VIEW ANY DEFINITION TO [$(collectionUser)]
+	GRANT VIEW SERVER STATE TO [$(collectionUser)]
+END;
+
+OPEN db_cursor  
+FETCH NEXT FROM db_cursor INTO @dbname  
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    BEGIN
+        exec ('
+        use [' + @dbname + '];
+        CREATE USER [$(collectionUser)] FOR LOGIN [$(collectionUser)];
+        GRANT VIEW DATABASE STATE TO [$(collectionUser)]');
+    END;
+
+    FETCH NEXT FROM db_cursor INTO @dbname;
+END;
+
+CLOSE db_cursor  
+DEALLOCATE db_cursor
