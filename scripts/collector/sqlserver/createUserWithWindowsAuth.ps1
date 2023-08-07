4550967de9c2619daefdf72dee7d6842b4410a2b
Shane Borden <shaneborden@google.com>
2023-08-07 15:09:55 -0400

    mark port parameter optional

--- scripts/collector/sqlserver/createUserWithWindowsAuth.ps1
+++ scripts/collector/sqlserver/createUserWithWindowsAuth.ps1
@@ -23,45 +23,35 @@
     Connection string usually in the form of [server name / ip address]\[instance name] (required)
 .PARAMETER port
     Connection port (optional)
-.PARAMETER user
-    SqlServer superuser username (required)
-.PARAMETER pass
-    SqlServer superuser password (required)
 .PARAMETER collectionUserName
     Collection username (optional)
 .PARAMETER collectionUserPass
     Collection username password (optional)
 .EXAMPLE
     To use a specific username / password combination:
-        C:\createuserwithsqluser.ps1 -serverName [server name / ip address]\[instance name] -user [superuser] -pass [superuser password] -collectionUserName [collection username] -collectionUserPass [collection username password]
+        C:\createuserwithwindowsauth.ps1 -serverName [server name / ip address]\[instance name] -collectionUserName [collection username] -collectionUserPass [collection username password]
     
     or
     
     To use default credentials:
-        C:\createuserwithsqluser.ps1 -serverName [server name / ip address]\[instance name] -user [superuser] -pass [superuser password]
+        C:\createuserwithwindowsauth.ps1 -serverName [server name / ip address]\[instance name]
 .NOTES
     https://googlecloudplatform.github.io/database-assessment/
 #>
 Param(
 [Parameter(Mandatory=$true)][string]$serverName="",
 [Parameter(Mandatory=$true)][string]$port="",
-[Parameter(Mandatory=$true)][string]$user="",
-[Parameter(Mandatory=$true)][string]$pass="",
 [Parameter(Mandatory=$false)][string]$collectionUserName="",
 [Parameter(Mandatory=$false)][string]$collectionUserPass=""
 )
+
+Import-Module $PSScriptRoot\dmaCollectorCommonFunctions.psm1
 
 if ([string]::IsNullorEmpty($serverName)) {
     Write-Output "Server parameter $serverName is empty.  Ensure that the parameter is provided"
     Exit 1
 } elseif ([string]::IsNullorEmpty($port)) {
     Write-Output "Server Admin Port parameter $port is empty.  Ensure that the parameter is provided"
-    Exit 1
-} elseif ([string]::IsNullorEmpty($user)) {
-    Write-Output "Server Admin Username parameter $user is empty.  Ensure that the parameter is provided"
-    Exit 1
-} elseif ([string]::IsNullorEmpty($pass)) {
-    Write-Output "Server Admin Username password parameter $pass is empty.  Ensure that the parameter is provided"
     Exit 1
 } elseif ([string]::IsNullorEmpty($collectionUserName)) {
     Write-Output "Collection Username parameter $collectionUserName is empty.  Ensure that the parameter is provided"
@@ -71,9 +61,6 @@ if ([string]::IsNullorEmpty($serverName)) {
     Exit 1
 }
 
-Write-Output "Creating Collection User in $serverName"
-
-
 if ([string]::IsNullorEmpty($port)) {
     Write-Output "Creating Collection User in $serverName"
     sqlcmd -S $serverName -i sql\createCollectionUser.sql -m 1 -v collectionUser=$collectionUserName collectionPass=$collectionUserPass
@@ -82,5 +69,3 @@ if ([string]::IsNullorEmpty($port)) {
     Write-Output "Creating Collection User in $serverName, using PORT $port"
     sqlcmd -S $serverName -i sql\createCollectionUser.sql -m 1 -v collectionUser=$collectionUserName collectionPass=$collectionUserPass
 }
-
-Exit 0
