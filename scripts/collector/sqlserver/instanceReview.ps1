f2b12aa6b7cf62f21ef47149494868dbfebe06e4
Cody Fincher <204685+cofin@users.noreply.github.com>
2023-08-07 20:51:15 +0000

    chore: bump to v4.3.13

--- scripts/collector/sqlserver/instanceReview.ps1
+++ scripts/collector/sqlserver/instanceReview.ps1
@@ -115,7 +115,7 @@ $instancename = $values[3]
 $current_ts = $values[4]
 $pkey = $values[5]
 
-$op_version = "4.3.12"
+$op_version = "4.3.13"
 
 if ($ignorePerfmon -eq "true") {
     $perfCounterLabel = "NoPerfCounter"
