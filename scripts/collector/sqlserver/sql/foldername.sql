2039d6d6adb75babcbf59cd2c2c768146e1043c1
Shane Borden <shaneborden@google.com>
2023-08-10 17:32:26 -0400

    Strip Slash from foldername

--- scripts/collector/sqlserver/sql/foldername.sql
+++ scripts/collector/sqlserver/sql/foldername.sql
@@ -19,18 +19,23 @@ SET NOCOUNT ON;
 SET LANGUAGE us_english;
 
 DECLARE @ASSESSMENT_DATABSE_NAME AS VARCHAR(256)
+DECLARE @MACHINE_NAME AS VARCHAR(256)
 
 SELECT @ASSESSMENT_DATABSE_NAME = N'$(database)';
 IF @ASSESSMENT_DATABSE_NAME = 'all'
    SELECT @ASSESSMENT_DATABSE_NAME = 'master'
 
+IF CHARINDEX('\', @@SERVERNAME)-1 = -1
+  SELECT @MACHINE_NAME = UPPER(@@SERVERNAME)
+ELSE
+  SELECT @MACHINE_NAME = UPPER(SUBSTRING(CONVERT(nvarchar, @@SERVERNAME),1,CHARINDEX('\', CONVERT(nvarchar, @@SERVERNAME))-1))
+
 BEGIN
 	exec('
 	SELECT CAST(SERVERPROPERTY(''ProductVersion'') AS VARCHAR(15)) AS Version,
-	UPPER(CONVERT(nvarchar, @@SERVERNAME)) as machinename,
+	''' + @MACHINE_NAME + ''' as machinename,
 	''' + @ASSESSMENT_DATABSE_NAME + ''' as databasename,
 	COALESCE(CONVERT(nvarchar, SERVERPROPERTY(''InstanceName'')), ''MSSQLSERVER'') as instancename,
 	replace(convert(varchar, getdate(),1),''/'','''') + replace(convert(varchar, getdate(),108),'':'','''') as current_ts,
-	UPPER(CONVERT(nvarchar, @@SERVERNAME)) + ''_'' + ''' + @ASSESSMENT_DATABSE_NAME + ''' + ''_'' +  COALESCE(CONVERT(nvarchar, SERVERPROPERTY(''InstanceName'')), ''MSSQLSERVER'') + ''_'' + replace(convert(varchar, getdate(),1),''/'','''') + replace(convert(varchar, getdate(),108),'':'','''') 
-	END as pkey');
+	''' + @MACHINE_NAME + ''' + ''_'' + ''' + @ASSESSMENT_DATABSE_NAME + ''' + ''_'' +  COALESCE(CONVERT(nvarchar, SERVERPROPERTY(''InstanceName'')), ''MSSQLSERVER'') + ''_'' + replace(convert(varchar, getdate(),1),''/'','''') + replace(convert(varchar, getdate(),108),'':'','''') as pkey');
 END
