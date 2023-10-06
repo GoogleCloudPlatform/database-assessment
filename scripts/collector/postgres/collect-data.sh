e70acce62bac30705010a628ec068b9cc837bd9d
Cody Fincher <204685+cofin@users.noreply.github.com>
2023-10-06 20:48:18 +0000

    chore: bump version to v4.3.22

--- scripts/collector/postgres/collect-data.sh
+++ scripts/collector/postgres/collect-data.sh
@@ -16,7 +16,7 @@
 
 ### Setup directories needed for execution
 #############################################################################
-OpVersion="4.3.21"
+OpVersion="4.3.22"
 
 LOCALE=$(echo $LANG | cut -d '.' -f 1)
 export LANG=C
