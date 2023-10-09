4f445a8d7dbc3f87dcf36e4114401a8b19e78ad0
wpuziewicz <35240389+wpuziewicz@users.noreply.github.com>
2023-10-09 09:09:54 -0500

    feat: HP-UX support (#305)
    * Fix for syntax of sed command on HP-UX.
    
    * Version bump
    
    * Version bump for 4.3.23

--- scripts/collector/sqlserver/instanceReview.ps1
+++ scripts/collector/sqlserver/instanceReview.ps1
@@ -129,7 +129,7 @@ $current_ts = $values[4]
 $pkey = $values[5]
 $dmaSourceId = $dmaSourceIdObj[0]
 
-$op_version = "4.3.22"
+$op_version = "4.3.23"
 
 if ($ignorePerfmon -eq "true") {
     $perfCounterLabel = "NoPerfCounter"
