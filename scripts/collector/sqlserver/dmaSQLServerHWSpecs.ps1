d3415520f243a68d8b1d2bc397be0f768e2b6a21
liozme <149305222+liozme@users.noreply.github.com>
2023-11-09 18:19:38 +0200

    feat: Adding dmaSQLServerHWSpecs.ps1 to collect the HW Shape info (#311)
    * Adding dmaSQLServerHWSpecs.ps1 to collect the HW Shape info
    
    * changing file name
    
    * adding credentials
    
    * adding credentials
    
    * Fix CR Comment - logical cores
    
    * Removing credential ask and will try to collect the data with WMI, if fails skipping
    
    * Adding logging to file
    
    * default log
    
    * CR Fixes 2
    
    * removing empty row, only headers
    
    * switch to set content
    
    * CR fixes same fields as DMA
    
    * MachineName

---
+++ scripts/collector/sqlserver/dmaSQLServerHWSpecs.ps1
@@ -0,0 +1,97 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+<#
+.SYNOPSIS
+    .
+.DESCRIPTION
+    Collects HW Specs using Get-WmiObject to be uploaded to Google Database Migration Assistant for MS SQL Server.
+.PARAMETER computerName
+    The target computer name to collect the HW specs from (Optional).
+.PARAMETER outputPath
+    The output full path of the csv that this scripts creates (Required).
+.PARAMETER pkey
+    Final Output Directory (Required).
+.PARAMETER dmaSourceId
+    DMA derived unique id (Required).
+.PARAMETER dmaManualId
+    Customer Manual Unique ID or dma manual unique id (Optional).
+.PARAMETER logLocation
+    Location of log file to output the script (Optional).
+.EXAMPLE
+    C:\dmaSQLServerHWSpecs.ps1 -computerName localhost -outputPath a.out -pkey pkey1 -dmaSourceId src1
+.NOTES
+    https://googlecloudplatform.github.io/database-assessment/
+#>
+param (
+	[Parameter(
+		Mandatory=$False,
+		HelpMessage="The computer name"
+	)][string]$computerName = $env:COMPUTERNAME,
+	[Parameter(
+		Mandatory=$True,
+		HelpMessage="The Output path"
+	)][string]$outputPath,
+	[Parameter(
+		Mandatory=$True,
+		HelpMessage="The pkey value"
+	)][string]$pkey,
+	[Parameter(
+		Mandatory=$True,
+		HelpMessage="The dma_source_id"
+	)][string]$dmaSourceId,
+	[Parameter(
+		Mandatory=$False,
+		HelpMessage="The dma_manual_id"
+	)][string]$dmaManualId="NA",
+	[Parameter(
+		Mandatory=$False,
+		HelpMessage="The log file location"
+	)][string]$logLocation="dmaSqlServerHWSpecs.log"
+)
+
+Import-Module $PSScriptRoot\dmaCollectorCommonFunctions.psm1
+
+try {
+	WriteLog -logLocation $logLocation -logMessage "Fetching machine HW specs from computer:$computerName and storing it in output:$outputPath" -logOperation "FILE"
+
+	# Physical cores count.
+	$PhysicalCpuCount=(Get-WmiObject Win32_Processor -ComputerName $computerName | Measure-Object -Property NumberOfCores -Sum).Sum
+
+	# Logical cores count.
+	$LogicalCpuCount=(Get-WmiObject Win32_Processor -ComputerName $computerName | Measure-Object -Property NumberOfLogicalProcessors -Sum).Sum
+	
+	# Total memory in bytes.
+	$memoryBytes=(Get-WmiObject Win32_PhysicalMemory -ComputerName $computerName | Measure-Object -Property Capacity -Sum).Sum
+	
+	# CSV data.
+	$csvData = [PSCustomObject]@{
+		"pkey" = $pkey
+		"dma_source_id" = $dmaSourceId
+		"dma_manual_id" = $dmaManualId
+		"MachineName" = $computerName
+		"PhysicalCpuCount" = $PhysicalCpuCount
+		"LogicalCpuCount" = $LogicalCpuCount
+		"TotalOSMemoryMB" = $memoryBytes/1024/1024
+	}
+	
+	# Writing to csv.
+	$csvData | Export-Csv -Path $outputPath -Delimiter "|" -NoTypeInformation -Encoding UTF8
+	WriteLog -logLocation $logLocation -logMessage "Successfully fetched machine HW specs of $computerName to output:$outputPath" -logOperation "FILE"	
+}
+catch {
+	WriteLog -logLocation $logLocation -logMessage "ERROR - Failed fetching machine HW specs of $computerName" -logOperation "FILE"	
+
+	# Writing Empty CSV File.
+	Set-Content -Path $outputPath -Encoding UTF8 -Value '"pkey"|"dma_source_id"|"dma_manual_id"|"MachineName"|"PhysicalCpuCount"|"LogicalCpuCount"|"TotalOSMemoryMB"'
+}
