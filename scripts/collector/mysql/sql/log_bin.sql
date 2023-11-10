4e0f5ffd512af69d5ac16f759f1e7acc504a259c
Cody Fincher <cody.fincher@gmail.com>
2023-11-10 13:22:43 -0600

    fix: remove trailing comma and reorder fields

--- scripts/collector/mysql/sql/log_bin.sql
+++ scripts/collector/mysql/sql/log_bin.sql
@@ -2,4 +2,4 @@ SELECT concat(char(39), @DMA_MANUAL_ID, char(39)) as PKEY,
     concat(char(39), @DMA_SOURCE_ID, char(39)) as DMA_SOURCE_ID,
     concat(char(39), @DMA_MANUAL_ID, char(39)) as DMA_MANUAL_ID,
     @@log_bin,
-    @@log_slave_updates,
+    @@log_slave_updates
