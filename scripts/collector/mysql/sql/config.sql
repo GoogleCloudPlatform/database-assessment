05acd84ee02ae57b724edbe9b94671f2f10e8c9c
Cody Fincher <cody.fincher@gmail.com>
2023-11-13 15:13:51 -0600

    feat: adds an example sample metric

--- scripts/collector/mysql/sql/config.sql
+++ scripts/collector/mysql/sql/config.sql
@@ -16,18 +16,32 @@ all_vars as (
             from information_schema.GLOBAL_VARIABLES
         )
 ),
+calculated_metrics as (
+    select 'IS_MARIADB' as variable_name,
+        case
+            when upper(gv.variable_value) like '%MARIADB%' then 1
+            else 0
+        end as variable_value
+    from information_schema.global_variables gv
+    where gv.variable_name = 'VERSION'
+),
 src as (
-    select distinct 'ALL_VARIABLES' as variable_category,
+    select 'ALL_VARIABLES' as variable_category,
         variable_name,
         variable_value
     from all_vars
     union
-    select distinct 'GLOBAL_STATUS' as variable_category,
+    select 'GLOBAL_STATUS' as variable_category,
         variable_name,
         variable_value
     from global_status
+    union
+    select 'CALCULATED_METRIC' as variable_category,
+        variable_name,
+        variable_value
+    from calculated_metrics
 )
-select concat(char(39), @DMA_MANUAL_ID, char(39)) as PKEY,
+select distinct concat(char(39), @DMA_MANUAL_ID, char(39)) as PKEY,
     concat(char(39), @DMA_SOURCE_ID, char(39)) as DMA_SOURCE_ID,
     concat(char(39), @DMA_MANUAL_ID, char(39)) as DMA_MANUAL_ID,
     src.variable_category,
