6cbc9013d2c4410bf3df8bf29e2df341b2cec908
Cody Fincher <cody.fincher@gmail.com>
2023-11-13 14:08:09 -0600

    feat: replace parameter files with `config` lookup

---
+++ scripts/collector/mysql/sql/config.sql
@@ -0,0 +1,35 @@
+with global_status as (
+    select upper(variable_name) as variable_name,
+        variable_value
+    from information_schema.GLOBAL_STATUS
+),
+all_vars as (
+    select upper(variable_name) as variable_name,
+        variable_value
+    from information_schema.GLOBAL_VARIABLES
+    union
+    select upper(variable_name),
+        COALESCE(SESSION_VALUE, GLOBAL_VALUE) variable_value
+    from information_schema.SYSTEM_VARIABLES
+    where variable_name not in (
+            select variable_name
+            from information_schema.GLOBAL_VARIABLES
+        )
+),
+src as (
+    select distinct 'ALL_VARIABLES' as variable_category,
+        variable_name,
+        variable_value
+    from all_vars
+    union
+    select distinct 'GLOBAL_STATUS' as variable_category,
+        variable_name,
+        variable_value
+    from global_status
+)
+select concat(char(39), @DMA_MANUAL_ID, char(39)) as PKEY,
+    concat(char(39), @DMA_SOURCE_ID, char(39)) as DMA_SOURCE_ID,
+    concat(char(39), @DMA_MANUAL_ID, char(39)) as DMA_MANUAL_ID,
+    src.variable_name,
+    src.variable_value
+from src;
--- scripts/collector/mysql/sql/log_bin.sql
+++
@@ -1,5 +0,0 @@
-SELECT concat(char(39), @DMA_MANUAL_ID, char(39)) as PKEY,
-    concat(char(39), @DMA_SOURCE_ID, char(39)) as DMA_SOURCE_ID,
-    concat(char(39), @DMA_MANUAL_ID, char(39)) as DMA_MANUAL_ID,
-    @@log_bin,
-    @@log_slave_updates
