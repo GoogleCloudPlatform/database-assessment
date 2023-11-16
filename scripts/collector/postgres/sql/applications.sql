7cc907a1096464ab7773b7f4fbfa4d7e44594037
liozme <149305222+liozme@users.noreply.github.com>
2023-11-16 12:04:56 +0200

    revert Update applications.sql

--- scripts/collector/postgres/sql/applications.sql
+++ scripts/collector/postgres/sql/applications.sql
@@ -1,8 +1,8 @@
-\o output/opdb__pg_applications_:VTAG.csv
+\ o output / opdb__pg_applications_ :VTAG.csv
 SELECT chr(39) || :PKEY || chr(39) as pkey,
     chr(39) || :DMA_SOURCE_ID || chr(39) AS dma_source_id,
     chr(39) || :DMA_MANUAL_ID || chr(39) AS dma_manual_id,
     application_name as application_name,
     count(*) as application_count
 FROM pg_stat_activity
-GROUP BY application_name
+GROUP BY 1
