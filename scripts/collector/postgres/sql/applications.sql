a6cf18973a87e9022a8de1a76730a007fc79dd93
Cody Fincher <cody.fincher@gmail.com>
2023-11-27 10:09:32 -0600

    fix: add quotes

--- scripts/collector/postgres/sql/applications.sql
+++ scripts/collector/postgres/sql/applications.sql
@@ -1,7 +1,7 @@
 select chr(39) || :DMA_SOURCE_ID || chr(39) as pkey,
     chr(39) || :DMA_SOURCE_ID || chr(39) as dma_source_id,
     chr(39) || :DMA_MANUAL_ID || chr(39) as dma_manual_id,
-    application_name as application_name,
-    count(*) as application_count
+    chr(39) || application_name || chr(39) as application_name,
+    chr(39) || count(*) || chr(39) as application_count
 from pg_stat_activity
 group by 1
