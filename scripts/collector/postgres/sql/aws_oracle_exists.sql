f36bc6c2238d3aded43c9a4e523c205ffa8cfe86
Cody Fincher <codyfincher@google.com>
2023-11-27 18:30:27 +0000

    feat: quote selected columns

--- scripts/collector/postgres/sql/aws_oracle_exists.sql
+++ scripts/collector/postgres/sql/aws_oracle_exists.sql
@@ -1,9 +1,9 @@
 select chr(39) || :PKEY || chr(39) as pkey,
   chr(39) || :DMA_SOURCE_ID || chr(39) as dma_source_id,
   chr(39) || :DMA_MANUAL_ID || chr(39) as dma_manual_id,
-  exists (
+  chr(39) || exists (
     select
     from information_schema.tables
     where table_schema = 'aws_oracle_ext'
       and TABLE_NAME = 'versions'
-  ) as sct_oracle_extension_exists
+  ) || chr(39) as sct_oracle_extension_exists
