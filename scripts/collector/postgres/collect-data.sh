b2ad788a18e80f72fcdf601b31432e4e825afc2f
Cody Fincher <codyfincher@google.com>
2023-12-04 22:03:54 +0000

    fix: get only the version number

--- scripts/collector/postgres/collect-data.sh
+++ scripts/collector/postgres/collect-data.sh
@@ -120,7 +120,7 @@ function checkVersionPg {
 
     # SELECT 'DMAFILETAG~' , version();
     dbVersion=$(PGPASSWORD="$pass" ${SQLCMD}  --user=$user -d $db -h $host -w -p $port -t --no-align << EOF
-SELECT current_setting('server_version');
+SELECT current_setting('server_version_num');
 EOF
 )
 echo 'DMAFILETAG~'${dbVersion}'|'${dbVersion}'_'${OpVersion}'_'${host}'-'${port}'_'${db}'_'${db}'_'$(date +%y%m%d%H%M%S)
