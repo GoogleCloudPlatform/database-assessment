3b968c8aecd295e7728f001ab05d84323892563c
Cody Fincher <cody.fincher@gmail.com>
2024-02-06 08:22:43 -0600

    feat: additional work

--- src/dma/cli/_utils.py
+++ src/dma/cli/_utils.py
@@ -4,13 +4,10 @@ from importlib.util import find_spec
 
 from rich import get_console
 
-RICH_CLICK_INSTALLED = find_spec("rich-click") is not None
-
-
 __all__ = (
     "RICH_CLICK_INSTALLED",
     "console",
 )
 
-
+RICH_CLICK_INSTALLED = find_spec("rich_click") is not None
 console = get_console()
--- src/dma/cli/main.py
+++ src/dma/cli/main.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
-from ._utils import RICH_CLICK_INSTALLED
+from ._utils import RICH_CLICK_INSTALLED, console
 
 if TYPE_CHECKING or not RICH_CLICK_INSTALLED:  # pragma: no cover
     import click
@@ -30,7 +30,114 @@ else:  # pragma: no cover
 __all__ = ("app_group",)
 
 
-@group(context_settings={"help_option_names": ["-h", "--help"]})
+@group(name="DMA", context_settings={"help_option_names": ["-h", "--help"]})
 @pass_context
 def app_group(ctx: Context) -> None:
-    ...
+    """Database Migration Assessment"""
+
+
+@app_group.command(
+    name="collect-data",
+    no_args_is_help=True,
+    short_help="Collect data from a source database..",
+)
+@click.option(
+    "--no-prompt",
+    help="Do not prompt for confirmation before executing check.",
+    type=bool,
+    default=False,
+    required=False,
+    show_default=True,
+    is_flag=True,
+)
+@click.option(
+    "--db-type",
+    "-db",
+    help="The type of the database to connect to",
+    default=None,
+    type=click.STRING,
+    required=False,
+    show_default=False,
+)
+def collect_data(
+    no_prompt: bool,
+    db_type: Literal["mysql", "postgres", "mssql", "oracle"] | None = None,
+) -> None:
+    """Process a collection of advisor extracts."""
+    console.rule("Starting data collection process", align="left")
+
+
+@app_group.command(
+    name="readiness-check",
+    no_args_is_help=True,
+    short_help="Execute the DMS migration readiness checklist.",
+)
+@click.option(
+    "--no-prompt",
+    help="Do not prompt for confirmation before executing check.",
+    type=bool,
+    default=False,
+    required=False,
+    show_default=True,
+    is_flag=True,
+)
+@click.option(
+    "--db-type",
+    "-db",
+    help="The type of the database to connect to",
+    default=None,
+    type=click.STRING,
+    required=False,
+    show_default=False,
+)
+@click.option(
+    "--username",
+    "-u",
+    help="The database user to connect as.",
+    default=None,
+    type=click.STRING,
+    required=False,
+    show_default=False,
+)
+@click.option(
+    "--password",
+    "-pw",
+    help="The database user password.",
+    default=None,
+    type=click.STRING,
+    required=False,
+    show_default=False,
+)
+@click.option(
+    "--hostname",
+    "-h",
+    help="The hostname of the database server",
+    default=None,
+    type=click.STRING,
+    required=False,
+    show_default=False,
+)
+@click.option(
+    "--port",
+    "-p",
+    help="The port of the database server",
+    default=None,
+    type=click.INT,
+    required=False,
+    show_default=False,
+)
+@click.option(
+    "--database",
+    "-d",
+    help="The name of the database to connect to.",
+    default=None,
+    type=click.STRING,
+    required=False,
+    show_default=False,
+)
+def readiness_check(
+    no_prompt: bool,
+    db_type: Literal["mysql", "postgres", "mssql", "oracle"] | None = None,
+) -> None:
+    """Process a collection of advisor extracts."""
+    console.rule("Starting readiness check process", align="left")
