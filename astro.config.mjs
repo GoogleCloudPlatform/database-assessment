e7caae43338bb8433c45931c101d55e606bc4941
Cody Fincher <codyfincher@google.com>
2024-02-09 17:12:44 +0000

    feat: updated build tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import mdx from "@astrojs/mdx"
-import singleFile from "astro-single-file"
 const ASSET_URL = process.env.ASSET_URL || "/"
 const VITE_PORT = process.env.VITE_PORT || "5173"
 const VITE_HOST = process.env.VITE_HOST || "localhost"
