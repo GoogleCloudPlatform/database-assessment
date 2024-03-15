26a0ba81c54296ba0815dc4e99dbe74ce303e2c2
wpuziewicz <warrenpuz@google.com>
2024-03-15 13:18:09 -0500

    Update comments in sql/extracts/eoj.sql

--- scripts/collector/oracle/sql/extracts/eoj.sql
+++ scripts/collector/oracle/sql/extracts/eoj.sql
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Google LLC
+Copyright 2024 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
--- This file intentionally left empty
+-- This file intentionally has no extraction SQL.
 spool &outputdir/opdb__eoj__&v_tag
 prompt END_OF_DMA_COLLECTION
 spool off
