afd50fbc083ad79a88c4225364f0cc73cfd9c24e
Cody Fincher <codyfincher@google.com>
2024-03-19 01:20:57 +0000

    chore: remove unused fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,15 +11,9 @@ Read more about conftest.py under:
 
 from __future__ import annotations
 
-import asyncio
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 import pytest
-
-if TYPE_CHECKING:
-    from collections import abc
-
 
 pytestmark = pytest.mark.anyio
 here = Path(__file__).parent
@@ -30,18 +24,3 @@ pytest_plugins = ["tests.docker_service_fixtures"]
 @pytest.fixture
 def anyio_backend() -> str:
     return "asyncio"
-
-
-@pytest.fixture(scope="session")
-def event_loop() -> "abc.Iterator[asyncio.AbstractEventLoop]":
-    """Scoped Event loop.
-
-    Need the event loop scoped to the session so that we can use it to check
-    containers are ready in session scoped containers fixture.
-    """
-    policy = asyncio.get_event_loop_policy()
-    loop = policy.new_event_loop()
-    try:
-        yield loop
-    finally:
-        loop.close()
