3f895da933c2598b7425ecf6e19ab059fe7adaa2
Steve Senior <ssenior45@googlemail.com>
2024-03-20 00:12:04 +0000

    feat: `Vite` + `Vue` + `Shadcn` Single Offline Page Prototype (#378)
    feat: single offline page prototype

---
+++ vite-project/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Toaster from '@/components/ui/toast/Toaster.vue'
+</script>
+
+<template>
+  <Toaster />
+  <HelloWorld msg="Vite + Vue + Shadcn Single Offline Page Prototype" />
+</template>
