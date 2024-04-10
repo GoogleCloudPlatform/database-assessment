7c8892f9f5719e81b3fe9dac80e73ace688caa43
Cody Fincher <codyfincher@google.com>
2024-04-10 21:40:16 +0000

    fix: adds placeholder query

--- scripts/collector/mysql/sql/5.7/resource_groups.sql
+++ scripts/collector/mysql/sql/5.7/resource_groups.sql
@@ -8,10 +8,10 @@ select concat(char(34), @PKEY, char(34)) as pkey,
     concat(char(34), src.vcpu_ids, char(34)) as vcpu_ids,
     concat(char(34), src.thread_priority, char(34)) as thread_priority
 from (
-        select rg.resource_group_type as resource_group_type,
-            rg.resource_group_enabled as resource_group_enabled,
-            rg.resource_group_name as resource_group_name,
-            rg.vcpu_ids as vcpu_ids,
-            rg.thread_priority as thread_priority,
-            from information_schema.resource_groups rg
+        select 'Unsupported Version Placeholder' as resource_group_type,
+            0 as resource_group_enabled,
+            'Placeholder Value' as resource_group_name,
+            '' as vcpu_ids,
+            0 as thread_priority
+        limit 0
     ) src;
