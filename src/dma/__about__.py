c6741388f2fe0710031e7b2cd1ff610e68e4e798
Cody Fincher <codyfincher@google.com>
2024-04-22 14:55:18 +0000

    feat: begin implementing a formal python config

---
+++ src/dma/__about__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, metadata, version
+
+__all__ = ("__project__", "__version__")
+
+try:
+    __version__ = version("dma")
+    """Version of the project."""
+    __project__ = metadata("dma")["Name"]
+    """Name of the project."""
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
+    __project__ = "Google Database Assessment"
+finally:
+    del version, PackageNotFoundError, metadata
