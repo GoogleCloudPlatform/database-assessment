52f4634c9b54894791ea7a6f488a0c8e696e48eb
Cody Fincher <codyfincher@google.com>
2024-04-24 15:18:12 +0000

    chore: remove deprecated config

--- scripts/build-binary-package.sh
+++ scripts/build-binary-package.sh
@@ -2,4 +2,4 @@
 # shellcheck disable=SC2086
 current_version=$(hatch version)
 hatch build
-PYAPP_PROJECT_PATH="$(ls ${PWD}/dist/dma-${current_version}-py3-none-any.whl)" PYAPP_PROJECT_FEATURES="oracle,postgres,mssql,mysql,server,remote" PYAPP_DISTRIBUTION_EMBED="1" hatch build -t app
+PYAPP_UV_ENABLED="1" PYAPP_PROJECT_PATH="$(ls ${PWD}/dist/dma-${current_version}-py3-none-any.whl)" PYAPP_PROJECT_FEATURES="oracle,postgres,mssql,mysql,server,remote" PYAPP_DISTRIBUTION_EMBED="1" hatch build -t app
