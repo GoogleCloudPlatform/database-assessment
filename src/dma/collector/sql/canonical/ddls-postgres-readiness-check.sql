87e53ea6d04bb43674b6fcb99d75664807381451
Cody Fincher <codyfincher@google.com>
2024-05-08 23:35:52 +0000

    feat: placeholder

--- src/dma/collector/sql/canonical/ddls-postgres-readiness-check.sql
+++ src/dma/collector/sql/canonical/ddls-postgres-readiness-check.sql
@@ -1,13 +1,13 @@
--- name: ddl-02-postgres-readiness check!
+-- name: ddl-postgres-02-readiness_check!
 create or replace table postgres_database_summary(
-        database_name varchar,
-        database_type varchar,
-        database_version varchar,
-        database_size numeric
-    );
+    database_name varchar,
+    database_type varchar,
+    database_version varchar,
+    database_size numeric
+  );
 
 create or replace table alloydb_readiness_check_summary(
-        severity ENUM ('INFO', 'WARNING', 'ERROR'),
-        assessment_type varchar,
-        info varchar
-    );
+    severity ENUM ('INFO', 'WARNING', 'ERROR'),
+    assessment_type varchar,
+    info varchar
+  );
