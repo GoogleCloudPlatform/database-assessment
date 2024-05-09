b51e46374afb9a4c6743bcb1dbc262b1acd63529
Cody Fincher <codyfincher@google.com>
2024-05-09 16:00:11 +0000

    fix: remove copy pasta

--- src/dma/collector/sql/canonical/readiness-check-postgres.sql
+++ src/dma/collector/sql/canonical/readiness-check-postgres.sql
@@ -19,13 +19,3 @@ create or replace table readiness_check_summary(
     assessment_type varchar,
     info varchar
   );
-
--- name: readiness-check-rules-01-
-select current_setting('server_version_num')::VARCHAR as db_version;
-
--- name: init-readiness-check-get-execution-id$
-select 'postgres_' || current_setting('server_version_num') || '_' || to_char(current_timestamp, 'YYYYMMDDHH24MISSMS') as execution_id;
-
--- name: init-get-source-id$
-select system_identifier::VARCHAR as source_id
-from pg_control_system();
