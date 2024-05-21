c702bce29d34f248851b4881abac8991ff3df531
Yair Halberstadt <yairhalberstadt@gmail.com>
2024-05-21 18:40:03 +0300

    Reduce error prominence in dmaSQLServerHWSpecs.ps1 (#440)
    * fix: simplify build (#438)
    
    * chore: fix cross compile and remove builds that aren't needed (#439)
    
    * Reduce error prominence in dmaSQLServerHWSpecs.ps1
    
    See b/340537974
    
    Previously:
    
    ```
    Get-WmiObject : The RPC server is unavailable.
    At C:\Users\SguegliL\CCSG2\dmaSQLServerHWSpecs.ps1:107 char:31
    + ... ata.PhysicalCpuCount = (Get-WmiObject Win32_Processor @params | Measu ...
    +                             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        + CategoryInfo          : InvalidOperation: (:) [Get-WmiObject], COMException
        + FullyQualifiedErrorId : GetWMICOMException,Microsoft.PowerShell.Commands.GetWmiObjectCommand
     
    Get-WmiObject : The RPC server is unavailable.
    At C:\Users\SguegliL\CCSG2\dmaSQLServerHWSpecs.ps1:110 char:30
    + ... Data.LogicalCpuCount = (Get-WmiObject Win32_Processor @params | Measu ...
    +                             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        + CategoryInfo          : InvalidOperation: (:) [Get-WmiObject], COMException
        + FullyQualifiedErrorId : GetWMICOMException,Microsoft.PowerShell.Commands.GetWmiObjectCommand                                                                                                                                                                                                                                                                                          Get-WmiObject : The RPC server is unavailable.                                                                                                                                                At C:\Users\SguegliL\CCSG2\dmaSQLServerHWSpecs.ps1:113 char:33                                                                                                                                + ... alOSMemoryBytes = (Get-WmiObject Win32_PhysicalMemory @params | Measu ...                                                                                                               +                        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        + CategoryInfo          : InvalidOperation: (:) [Get-WmiObject], COMException
        + FullyQualifiedErrorId : GetWMICOMException,Microsoft.PowerShell.Commands.GetWmiObjectCommand
     
    Get-WmiObject : The RPC server is unavailable.
    At C:\Users\SguegliL\CCSG2\dmaSQLServerHWSpecs.ps1:119 char:25
    + ... Data.PrimaryMAC = (Get-WmiObject Win32_NetworkAdapter @params | Where ...
    +                        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        + CategoryInfo          : InvalidOperation: (:) [Get-WmiObject], COMException
        + FullyQualifiedErrorId : GetWMICOMException,Microsoft.PowerShell.Commands.GetWmiObjectCommand
     
    Get-WmiObject : The RPC server is unavailable.
    At C:\Users\SguegliL\CCSG2\dmaSQLServerHWSpecs.ps1:122 char:26
    + ... ddresses = (Get-WmiObject Win32_NetworkAdapterConfiguration @params | ...
    +                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        + CategoryInfo          : InvalidOperation: (:) [Get-WmiObject], COMException
        + FullyQualifiedErrorId : GetWMICOMException,Microsoft.PowerShell.Commands.GetWmiObjectCommand
    ```
    
    After these changes:
    ```
    ERROR - Failed fetching machine HW specs of not-a-vm : The RPC server is unavailable.
    ```
    
    ---------
    
    Co-authored-by: Cody Fincher <204685+cofin@users.noreply.github.com>
    Co-authored-by: Shane Borden <sborden76@gmail.com>

--- scripts/build-binary-package.sh
+++ scripts/build-binary-package.sh
@@ -5,4 +5,4 @@ hatch build
 PYAPP_PYTHON_VERSION="3.12" PYAPP_DISTRIBUTION_VARIANT="v1" PYAPP_UV_ENABLED="1" PYAPP_FULL_ISOLATION="1" PYAPP_DISTRIBUTION_EMBED="1" \
     PYAPP_PROJECT_PATH="$(ls ${PWD}/dist/dma-${current_version}-py3-none-any.whl)" \
     PYAPP_PROJECT_FEATURES="oracle,postgres,mssql,mysql,server,remote" \
-    hatch build -t app
+    hatch build -t binary
