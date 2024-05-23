83872b013958c389294bb5ee58626bf022fe363b
Cody Fincher <codyfincher@google.com>
2024-05-23 17:58:14 +0000

    chore: ignore ERA001 for a line

--- src/dma/collector/workflows/base.py
+++ src/dma/collector/workflows/base.py
@@ -47,9 +47,8 @@ class BaseWorkflow:
                 self.local_db.execute(
                     f"create or replace table {table_name} as select {', '.join(column_name for column_name in column_names)} from obj_{table_name}"  # noqa: S608
                 )
-                # self.local_db.execute(
-                #     f"insert into {table_name}({', '.join(column_name for column_name in column_names)}) select {', '.join(column_name for column_name in column_names)} from obj_{table_name}"
-                # )
+                # self.local_db.execute( f"insert into {table_name}({', '.join(column_name for column_name in column_names)}) select {', '.join(column_name for column_name in column_names)} from obj_{table_name}")  # noqa: ERA001
+
                 self.local_db.execute(f"drop view obj_{table_name}")
 
     def dump_database(self, export_path: Path, delimiter: str = "|") -> None:
