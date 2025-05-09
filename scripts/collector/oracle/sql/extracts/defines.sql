172e29a604fbe00768aa3bee0e8ed668b987832d
Warren Puziewicz <warrenpuz@google.com>
2025-05-09 09:21:43 -0500

    Clean up the defines dump

--- scripts/collector/oracle/sql/extracts/defines.sql
+++ scripts/collector/oracle/sql/extracts/defines.sql
@@ -18,36 +18,21 @@
 PROMPT "SUBSTITUTION VARIABLES"
 PROMPT AWRDIR                        = &AWRDIR.
 PROMPT EXTRACTSDIR                   = extracts
-PROMPT SQLDIR                        = &SQLDIR.
 PROMPT STATSPACKDIR                  = &STATSPACKDIR.
-PROMPT TERMOUTOFF                    = &TERMOUTOFF.
-PROMPT dmaVersion                    = &dmaVersion.
-PROMPT p_sp_script                   = &p_sp_script.
-PROMPT p_statsWindow                 = &p_statsWindow.
 PROMPT s_a_con_id                    = &s_a_con_id.
 PROMPT s_b_con_id                    = &s_b_con_id.
 PROMPT s_c_con_id                    = &s_c_con_id.
--- PROMPT s_app_join_cond               ='&s_app_join_cond.'
--- PROMPT s_app_join_dbsum_cond         ='&s_app_join_dbsum_cond.'
 PROMPT s_cdb_join_cond               = &s_cdb_join_cond.
 PROMPT s_compress_col                = &s_compress_col.
 PROMPT s_d_con_id                    = &s_d_con_id.
 PROMPT s_db_container_col            = &s_db_container_col.
 PROMPT s_db_unique_name              = &s_db_unique_name.
-PROMPT s_dbid                        = &s_dbid.
---PROMPT s_dbname                      = &s_dbname.
 PROMPT s_dbparam_dflt_col            = &s_dbparam_dflt_col.
---PROMPT s_dbversion                   = &s_dbversion.
 PROMPT s_dg_valid_role               = &s_dg_valid_role.
 PROMPT s_dg_verify                   = &s_dg_verify.
 PROMPT s_editionable_col             = &s_editionable_col.
-PROMPT s_h_con_id                    = &s_h_con_id.
---PROMPT s_hora                        = &s_hora.
---PROMPT s_host                        = &s_host.
 PROMPT s_index_visibility            = &s_index_visibility.
-PROMPT s_info_prompt                 = &s_info_prompt.
 PROMPT s_io_function                 = &s_io_function_sql.
---PROMPT s_inst                        = &s_inst.
 PROMPT s_is_container                = &s_is_container.
 PROMPT s_lob_compression_col         = &s_lob_compression_col.
 PROMPT s_lob_dedup_col               = &s_lob_dedup_col.
@@ -55,17 +40,10 @@ PROMPT s_lob_part_compression_col    = &s_lob_part_compression_col.
 PROMPT s_lob_part_dedup_col          = &s_lob_part_dedup_col.
 PROMPT s_lob_subpart_compression_col = &s_lob_subpart_compression_col.
 PROMPT s_lob_subpart_dedup_col       = &s_lob_subpart_dedup_col.
-PROMPT s_manualUniqueId              = &s_manualUniqueId.
-PROMPT s_max_snapid                  = &s_max_snapid.
-PROMPT s_max_snaptime                = &s_max_snaptime.
-PROMPT s_min_snapid                  = &s_min_snapid.
-PROMPT s_min_snaptime                = &s_min_snaptime.
-PROMPT s_name                        = &s_name.
 PROMPT s_p_con_id                    = &s_p_con_id.
 PROMPT s_pdb_join_cond               = &s_pdb_join_cond.
 PROMPT s_platform_name               = &s_platform_name.
 PROMPT s_pluggablelogging            = &s_pluggablelogging.
-PROMPT s_tag                         = &s_tag.
 PROMPT s_tblprefix                   = &s_tblprefix.
 PROMPT s_umf_test                    = &s_umf_test.
 PROMPT s_statssrc                    = &s_statssrc.
@@ -84,18 +62,9 @@ PROMPT s_t_segment_created           = &s_t_segment_created.
 PROMPT s_tp_segment_created          = &s_tp_segment_created. 
 PROMPT s_tsp_segment_created         = &s_tsp_segment_created.
 
-
-
 PROMPT
 PROMPT "BIND VARIABLES"
 BEGIN
---dbms_output.put_line( 'compress_for                  = ' ||  :compress_for);
---dbms_output.put_line( 'editionable                   = ' ||  :editionable);
---dbms_output.put_line( 'v_maxsnap                     = ' ||  :v_maxsnap);
---dbms_output.put_line( 'v_maxsnaptime                 = ' ||  :v_maxsnaptime);
---dbms_output.put_line( 'v_minsnap                     = ' ||  :v_minsnap);
---dbms_output.put_line( 'v_minsnaptime                 = ' ||  :v_minsnaptime);
---dbms_output.put_line( 'visibility                    = ' ||  :visibility);
 dbms_output.put_line( 'lv_cdb_join_cond              = ' ||  :lv_cdb_join_cond);
 dbms_output.put_line( 'lv_db_container_col           = ' ||  :lv_db_container_col);
 dbms_output.put_line( 'lv_tenancy                    = ' ||  :lv_tenancy);
