9005c1af3a0007c32767ac6514ad3bd4f7a313a8
Warren Puziewicz <warrenpuz@google.com>
2025-08-27 10:14:55 -0500

    Initial version of make_dist.sh

---
+++ scripts/collector/oracle/make_dist.sh
@@ -0,0 +1,213 @@
+#!/bin/bash 
+
+# Generate all combinations of single-purpose collectors.
+OUTPUTBASEDIR=dist
+
+dbver=""
+tenant=""
+stats=""
+statsdir=""
+umf=""
+
+SED='sed -i '
+if [[ "Darwin" = "$(uname)" ]];
+then
+  SED='sed -i bak'
+fi
+
+AWKCMD='
+print $0
+'
+
+# Copy the specified file to the target location and replace all SQL*Plus substitution variables with values from the parameter files.
+function gen_file {
+    fname="$1"
+    destdir="$2"
+    fdir=$(dirname ${fname})
+    mkdir -p ${destdir}/${fdir}
+    outfile=${destdir}/${fname}
+    cp ${fname} ${outfile}
+    sedsep=""
+    sedcmd=""
+
+    # The order of application is important.  Do not change the order of the variable* files.
+    for varfile in variables_${dbver}.txt variables_${umf}.txt  variables_${tenant}.txt variables_${stats}.txt variables_ALL.txt
+    do 
+       sedcmd=""
+       sedsep=""
+       while read -r subvarline 
+       do
+         subvar=$(echo ${subvarline} | cut -d ':' -f 1 | tr -d ' ' | tr -d '&' | tr -d '.' )
+         subval=$(echo ${subvarline} | cut -d ':' -f 2 | sed 's/^ *//;s/ *$//' )
+         # echo ${subvar} '=' ${subval}
+         sedcmd="${sedcmd}${sedsep}s/\&${subvar}\./${subval}/g"
+         sedsep=";"
+       done < <(grep -h -v -e '^#' -e '^--' -e '^$'  ${varfile}  )
+       sed -i bak  "${sedcmd}" ${outfile} 
+       rm ${outfile}bak 
+    done
+
+}
+
+# Replace all SQL*Plus includes with the contents of the file
+function replace_includes {
+    fname="$1"
+    destdir="$2"
+    fdir=$(dirname ${fname})
+
+    awk -v basedir=${destdir} -f- $1 > awktmp <<'EOF'
+/^@/ {
+  filename = substr($0, 2) 
+  gsub(/^[[:space:]]+|[[:space:]]+$/, "", filename)
+  if (filename != "") {
+    filename = basedir "/"  filename
+    while ((getline line < filename) > 0) {
+       if (line !~ /^--/)  {print line}
+    }
+    close(filename)
+  } else {
+    print $0 
+  }
+  next 
+}
+{
+  print $0 
+}
+EOF
+     mv awktmp ${fname}
+}
+
+function make_target {
+  line="${1}"
+  dbver=$(echo $line | cut -d ',' -f 1)
+  tenant=$(echo $line | cut -d ',' -f 2)
+  stats=$(echo $line | cut -d ',' -f 3)
+  statsdir=$(echo $stats | tr [:upper:] [:lower:])
+  umf=$(echo $line | cut -d ',' -f 4)
+  outputdir=${OUTPUTBASEDIR}/${dbver}/${tenant}/${stats}/${umf}/oracle
+
+  echo Generating :  $dbver $tenant $stats $umf $outputdir
+
+  echo Writing to : ${outputdir}
+  mkdir -p ${outputdir}
+
+  # Generate output files from the extracts directory.
+  for fname in $(find sql/extracts -maxdepth 1 -type f )
+  do
+    gen_file "${fname}" "${outputdir}"
+  done
+
+  # Generate output files for the chosen stats type
+  for fname in $(find sql/extracts/${statsdir} -maxdepth 1 -type f )
+  do
+    gen_file "${fname}" "${outputdir}"
+  done
+
+  # Copy other files needed for testing with the existing collector scripts.
+  cp  sql/op_sed_cleanup.sed ${outputdir}/sql
+  cp  sql/op_set_sql_env.sql ${outputdir}/sql
+
+  # Copy the conditional driving SQL files for the specified tenancy
+  case "${tenant}" in
+   "MULTI")
+      gen_file sql/op_collect_tenancy_multi.sql "${outputdir}"
+      ;;
+   "SINGLE")
+      gen_file sql/op_collect_tenancy_single.sql "${outputdir}"
+      ;;
+  esac
+
+  # Copy the conditional driving SQL files for the specified stats type
+  case "${stats}" in
+   "AWR")
+      gen_file sql/op_collect_stats_awr.sql "${outputdir}"
+      gen_file sql/extracts/statspack/awrhistosstat.sql "${outputdir}"
+      cp  sql/prompt_awr.sql ${outputdir}/sql
+      ;;
+   "NOSTATS")
+      cp  sql/op_collect_stats_nostats.sql ${outputdir}/sql
+      cp  sql/prompt_nostats.sql ${outputdir}/sql
+      ;;
+   "STATSPACK")
+      gen_file sql/op_collect_stats_statspack.sql "${outputdir}"
+      cp  sql/prompt_statspack.sql ${outputdir}/sql
+      ;;
+  esac
+
+  # Copy the remaining files
+  cp -r sql/setup ${outputdir}/sql
+  cp sql/op_collect_init.sql ${outputdir}/sql
+  cp collect-data.sh ${outputdir}
+  cp README.txt ${outputdir}
+
+  # Copy the automation directory.  (Exclude this? )
+  #cp -r automation ${outputdir}
+
+  # Generate the driver SQL files.
+  gen_file sql/op_collect.sql "${outputdir}"
+
+  # Replace all the includes with the contents of the referenced file.
+  for fname in $(grep -l -r '@' ${outputdir}/sql/extracts)
+  do 
+    replace_includes $fname ${outputdir}
+  done
+}
+
+
+### Validate input
+
+
+ if [[ $(($# & 1)) == 1 ]] ;
+ then
+  echo "Invalid number of parameters "
+  printUsage
+  exit
+ fi
+
+ while (( "$#" )); do
+	 if   [[ "$1" == "--maxParallel" ]];           then MAXPARALLEL="${2}"
+	 else if   [[ "$1" == "--configFile" ]];       then CONFIGFILE="${2}"
+	 else if   [[ "$1" == "--target" ]];           then TARGET="${2}"
+	 else
+		 echo "Unknown parameter ${1}"
+		 printUsage
+		 exit
+	 fi
+         fi
+         fi
+	 shift 2
+ done
+
+if [[ "$MAXPARALLEL" = "" ]];
+  then MAXPARALLEL=1
+fi
+
+if [[ "$TARGET" = "" ]];
+  then TARGET=".*"
+  else echo Making "${TARGET}"   
+       make_target "${TARGET}"
+       exit
+fi
+
+if [[ "$CONFIGFILE" = "" ]];
+  then CONFIGFILE="build.config"
+fi
+
+# Build all the version-specific SQL files
+for line in $(grep -v '#' $CONFIGFILE | grep -v "^$" | grep "$TARGET" )
+do
+ ./make_dist.sh --target "${line}" &
+ # Wait a couple of seconds before starting another build.
+ sleep 2
+ 
+ # Do not run another biuld if there are too many running already
+ while [[ $(ps -ef | grep make_dist.sh | grep -v grep | grep target | wc -l) -ge ${MAXPARALLEL} ]]
+ do
+  echo sleeping for 10 secs while waiting on builds:
+  ps | grep make_dist.sh | grep -v grep 
+
+  sleep 10
+ done
+done 
+echo Done
+ 
