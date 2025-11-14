c93a8f56915960079a016171d5e911551b2fcf4d
Warren Puziewicz <warrenpuz@google.com>
2025-11-14 15:52:31 -0600

    File renames.

---
+++ scripts/collector/oracle/sql/extracts/app_cloud.sql
@@ -0,0 +1,23 @@
+spool &outputdir/opdb__app_cloud__&v_tag
+WITH app_cloud as 
+@&EXTRACTSDIR/app_schemas.sql
+)
+SELECT 'EBS_OWNER:                ' || ebs_owner
+FROM app_cloud
+UNION
+SELECT 'SIEBEL_OWNER:             ' || siebel_owner
+FROM app_cloud
+UNION
+SELECT 'PSFT_OWNER:               ' || psft_owner
+FROM app_cloud
+UNION
+SELECT 'RDS_FLAG:                 ' || rds_flag
+FROM app_cloud
+UNION
+SELECT 'OCI_AUTONOMOUS_FLAG:      ' || oci_autonomous_flag
+FROM app_cloud
+UNION
+SELECT 'DBMS_CLOUD_PKG_INSTALLED: ' || dbms_cloud_pkg_installed
+FROM app_cloud;
+spool off
+
