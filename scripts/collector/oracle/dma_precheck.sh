9f91a89747e5fa19771848ca3719684a0d5d56c6
Warren Puziewicz <warrenpuz@google.com>
2026-01-06 08:18:29 -0600

    Mark code for Oracle Estate Explorer as 'Reserved for future use.'

--- scripts/collector/oracle/dma_precheck.sh
+++ scripts/collector/oracle/dma_precheck.sh
@@ -259,22 +259,23 @@ function precheckConfigFileFormat() {
         failcount=$(( $failcount + 1 ))
       fi
 
-      # Check parameters for OEE if given
-      if [[ "${oee_flag}" != ""  ]] ; then
-        if [[ "${oee_flag}" != "Y" ]] && [[ "${oee_flag}" != "N" ]] ; then
-          echo "FAILED : Invalid entry ${oee_flag} for OEE Flag on line ${lineno}. Must be one of (Y, N)."
-          failcount=$(( $failcount + 1 ))
-        fi
-
-        if [[ "${oee_flag}" == "Y" ]] ; then
-          oee_entries=1
-        fi
-
-        if [[ "${oee_flag}" = "Y" ]] && [[ ! -f ${oee_dir}/oee_group_extract-SA.sh ]] ; then
-            echo "FAILED : OEE collection is specified on line ${lineno} but the OEE collection files are not installed in ${oee_dir}.  Either install OEE to the specified location or set this flag to N in the configuration file."
-            failcount=$(( $failcount + 1 ))
-        fi
-      fi
+# RESERVED FOR FUTURE USE
+#      # Check parameters for OEE if given
+#      if [[ "${oee_flag}" != ""  ]] ; then
+#        if [[ "${oee_flag}" != "Y" ]] && [[ "${oee_flag}" != "N" ]] ; then
+#          echo "FAILED : Invalid entry ${oee_flag} for OEE Flag on line ${lineno}. Must be one of (Y, N)."
+#          failcount=$(( $failcount + 1 ))
+#        fi
+#
+#        if [[ "${oee_flag}" == "Y" ]] ; then
+#          oee_entries=1
+#        fi
+#
+#        if [[ "${oee_flag}" = "Y" ]] && [[ ! -f ${oee_dir}/oee_group_extract-SA.sh ]] ; then
+#            echo "FAILED : OEE collection is specified on line ${lineno} but the OEE collection files are not installed in ${oee_dir}.  Either install OEE to the specified location or set this flag to N in the configuration file."
+#            failcount=$(( $failcount + 1 ))
+#        fi
+#      fi
 
     fi
   done < <( tr -d ' ' < "${configuration_file}" | tr -d "${tab_char}" | ${grep_cmd} -v '^#' | ${grep_cmd} -v '^$' )
@@ -404,7 +405,7 @@ function precheckStats() {
     username=$(echo "${user}" | cut -d '/' -f 1)
     echo  "...Checking available performance statistics on database ${db} user ${username} for ${statssrc}"
     retcd=$(checkStats "${user}" "${db}" "${statssrc}" "${statswindow}")
-    success=$(echo "${retcd}" | ${grep_cmd} -e SUCCESS -e NONE -e WARNING | cut -d ' ' -f 1)
+    success=$(echo "${retcd}" | ${grep_cmd} -e SUCCESS -e NONE -e WARNING -e FAIL | cut -d ' ' -f 1)
     if [[ "${success}" = "SUCCESS" ]]; then
       successes+=("${retcd}")
     else
@@ -418,7 +419,6 @@ function precheckStats() {
         fi
       fi
     fi
-#  done < <( ${sed_cmd} "s/ //g;s/${tab_char}//g" "$configuration_file"  )
   done < <( tr -d ' ' < "${configuration_file}" | tr -d "${tab_char}" | ${grep_cmd} -v '^#' | ${grep_cmd} -v '^$' )
 
   retcd=${#successes[@]}
@@ -617,7 +617,8 @@ function runAllChecks() {
     if [[ $retval -eq 0 ]]; then precheckUser; retval=$?; fi
     if [[ $retval -eq 0 ]]; then precheckStats; retval=$?; fi
   fi
-  if [[ $retval -eq 0 ]] && [[ $oee_entries -eq 1 ]] ; then precheck_oee_platform; retval=$?; fi
+# RESERVED FOR FUTURE USE
+#  if [[ $retval -eq 0 ]] && [[ $oee_entries -eq 1 ]] ; then precheck_oee_platform; retval=$?; fi
 
   print_separator
 
